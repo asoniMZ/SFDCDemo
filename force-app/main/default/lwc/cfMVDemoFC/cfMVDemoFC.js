bc1076fc8ca2fae6f20ebcff903fb9e2c5b86357
asoniMZ <asoni@mindzcloud.com>
2023-05-18 15:21:26 +0530

    First Commit

---
+++ force-app/main/default/lwc/cfMVDemoFC/cfMVDemoFC.js
@@ -0,0 +1,64 @@
+import { FlexCardMixin } from "omnistudio/flexCardMixin";
+    import {interpolateWithRegex, interpolateKeyValue, loadCssFromStaticResource } from "omnistudio/flexCardUtility";
+    
+          import { LightningElement, api, track, wire } from "lwc";
+          import pubsub from "omnistudio/pubsub";
+          import { getRecord } from "lightning/uiRecordApi";
+          
+          import data from "./definition";
+          
+          import styleDef from "./styleDefinition";
+              
+          export default class cfMVDemoFC extends FlexCardMixin(LightningElement){
+              @api debug;
+              @api recordId;
+              @api objectApiName;
+              
+              @track record;
+              @track _sessionApiVars = {};
+        @api set cfAccRecId(val) {
+          if(typeof val !== "undefined") {
+            this._sessionApiVars["AccRecId"] = val;
+          }
+        } get cfAccRecId() {
+          return this._sessionApiVars["AccRecId"] || "undefined";
+        }
+      
+              
+              pubsubEvent = [];
+              customEvent = [];
+              
+              connectedCallback() {
+                super.connectedCallback();
+                this.setStyleDefinition(styleDef);
+                data.Session = {} //reinitialize on reload
+                
+                
+                
+                this.setDefinition(data);
+ this.registerEvents();
+                
+                
+              }
+              
+              disconnectedCallback(){
+                super.disconnectedCallback();
+                    
+                    
+
+                  this.unregisterEvents();
+              }
+
+              registerEvents() {
+                
+              }
+
+              unregisterEvents(){
+                
+              }
+            
+              renderedCallback() {
+                super.renderedCallback();
+                
+              }
+          }
